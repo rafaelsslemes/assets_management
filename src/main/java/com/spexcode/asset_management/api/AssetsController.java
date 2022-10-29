1ee1cb4ece796013604aea0ca0b0a422c26294b6
Rafael Lemes <rafael.sslemes@gmail.com>
2022-10-29 08:14:51 -0300

    Changed GetAll to respond HttpStatus

--- src/main/java/com/spexcode/asset_management/api/AssetsController.java
+++ src/main/java/com/spexcode/asset_management/api/AssetsController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +30,13 @@ public class AssetsController {
     }
 
     @GetMapping
-    public Iterable<Asset> getFromDatabase() {
-        return service.getAll();
+    public ResponseEntity<Iterable<Asset>> getFromDatabase() {
+
+        // Complete sigature
+        //return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+
+        // Short signature
+        return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
