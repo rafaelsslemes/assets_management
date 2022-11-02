ec4c92c7a7d6708b9c6f83011420ffbbe7795491
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-02 07:51:53 -0300

    Added Crud Tests

--- src/test/java/com/spexcode/asset_management/AssetManagementApplicationTests.java
+++ src/test/java/com/spexcode/asset_management/AssetManagementApplicationTests.java
@@ -1,13 +1,70 @@
 package com.spexcode.asset_management;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spexcode.asset_management.api.AssetsService;
+import com.spexcode.asset_management.model.Asset;
 
 @SpringBootTest
 class AssetManagementApplicationTests {
+	
+	@Autowired
+	AssetsService service;
 
 	@Test
-	void contextLoads() {
+	void registerAsset() {
+		Asset registered = service.register(baseAsset());
+		assertNotNull(registered.getId());
 	}
+
+	@Test
+	void getAsset() {
+		Asset registered = service.register(baseAsset());
+		Optional<Asset> fromDB = service.getById(registered.getId());
+		
+		assertTrue(fromDB.isPresent());
+		assertEquals(registered.getId(), fromDB.get().getId());
+	}
+
+	@Test
+	void deleteAsset() {
+		Asset registered = service.register(baseAsset());
+		service.delete(registered.getId());
+		Optional<Asset> fromDB = service.getById(registered.getId());
+		
+		assertFalse(fromDB.isPresent());
+	}
+
+	@Test
+	void updateAsset() {
+		Asset registered = service.register(baseAsset());
+		String description = registered.getDescription();
+
+		registered.setDescription("New Text");
+		service.update(registered);
+
+		Optional<Asset> fromDB = service.getById(registered.getId());
+		
+		assertEquals(registered.getId(), fromDB.get().getId());
+		assertNotEquals(description, fromDB.get().getDescription());
+	}
+
+	private Asset baseAsset() {
+		Asset asset = new Asset();
+		asset.setDescription("Trash Bucket");
+		asset.setType("furniture");
+		return asset;
+	}
+
 
 }
