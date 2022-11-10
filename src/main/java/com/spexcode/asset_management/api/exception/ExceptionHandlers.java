04077c0876183cd8778eec12d877cc43f17fc4e9
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-10 17:48:32 -0300

    Override method to hide StackTrace

--- src/main/java/com/spexcode/asset_management/api/exception/ExceptionHandlers.java
+++ src/main/java/com/spexcode/asset_management/api/exception/ExceptionHandlers.java
@@ -1,12 +1,21 @@
 package com.spexcode.asset_management.api.exception;
 
+import java.util.Set;
+
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionHandlers {
+public class ExceptionHandlers extends ResponseEntityExceptionHandler{
     
     @ExceptionHandler({EmptyResultDataAccessException.class})
     public ResponseEntity<String> handleEmptyResultDataAccess(Exception ex){
@@ -17,4 +26,10 @@ public class ExceptionHandlers {
     public ResponseEntity<String> handleIlegalArgument(Exception ex){
         return ResponseEntity.badRequest().build();
     }
+
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		return new ResponseEntity<>("METHOD_NOT_ALLOWED", HttpStatus.METHOD_NOT_ALLOWED);
+	}
 }
