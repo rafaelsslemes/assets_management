abaefabb2eca171edead18fe05717bec5322a2cd
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-15 08:32:03 -0300

    CSRF disabled

--- src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
+++ src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.spexcode.asset_management.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
         http.addFilterAfter(new SecurityFilter(), BasicAuthenticationFilter.class);
+        http.csrf().disable();
         return http.build();
     }
     
