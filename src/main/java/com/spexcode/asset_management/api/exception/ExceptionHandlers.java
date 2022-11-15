3af0d38cc6032481f4200dc453ffaa507b9af2b3
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-15 09:46:59 -0300

    Enabled MethodSecured

--- src/main/java/com/spexcode/asset_management/api/exception/ExceptionHandlers.java
+++ src/main/java/com/spexcode/asset_management/api/exception/ExceptionHandlers.java
@@ -4,6 +4,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +22,11 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler{
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<String> handleIlegalArgument(Exception ex){
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<String> handleAccessDenied(Exception ex){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("ACCESS DENIED");
     }
 
     // Override method to hide StackTrace
--- src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
+++ src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
@@ -2,15 +2,38 @@ package com.spexcode.asset_management.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig{
 
+    @Bean
+    public UserDetailsService users() {
+	    UserDetails user = User.builder()
+		.username("api_user")
+		.password(passwordEncoder().encode("123456"))
+		.roles("USER")
+		.build();
+        
+	    UserDetails admin = User.builder()
+		.username("api_admin")
+		.password(passwordEncoder().encode("123456"))
+		.roles("USER", "ADMIN")
+		.build();
+	return new InMemoryUserDetailsManager(user, admin);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,6 +42,11 @@ public class SecurityConfig{
         http.addFilterAfter(new SecurityFilter(), BasicAuthenticationFilter.class);
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+     return new BCryptPasswordEncoder();
     }
     
 }
