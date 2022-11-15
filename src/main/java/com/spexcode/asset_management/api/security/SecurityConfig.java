51cea4c1b5bb4e4a0119b28a1aa72a3c8dfcc57e
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-15 10:21:10 -0300

    Implemented an UserDetailService to provide authentication

--- src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
+++ src/main/java/com/spexcode/asset_management/api/security/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.spexcode.asset_management.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -19,21 +15,8 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig{
 
-    @Bean
-    public UserDetailsService users() {
-	    UserDetails user = User.builder()
-		.username("api_user")
-		.password(passwordEncoder().encode("123456"))
-		.roles("USER")
-		.build();
-        
-	    UserDetails admin = User.builder()
-		.username("api_admin")
-		.password(passwordEncoder().encode("123456"))
-		.roles("USER", "ADMIN")
-		.build();
-	return new InMemoryUserDetailsManager(user, admin);
-    }
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -44,9 +27,4 @@ public class SecurityConfig{
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder (){
-     return new BCryptPasswordEncoder();
-    }
-    
 }
